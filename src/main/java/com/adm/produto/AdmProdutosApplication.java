bc5266373dd4640cdf12c2cc9519f1e59c466780
silviney <silvineyf@gmail.com>
2022-10-18 20:35:19 -0300

    1° commit- criação do projeto

---
+++ src/main/java/com/adm/produto/AdmProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.adm.produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adm/produto/AdmProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adm.produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
